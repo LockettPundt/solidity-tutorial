a58f60adb41d188468f5d6946db52ffd508d4c04
Lockett Pundt <kalifornium@hotmail.com>
2022-03-29 23:25:41 -0400

    feat(Contracts): :sparkles: buy token functions. basic state variables

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./Token.sol";
+
 contract EthSwap {
   string public name = "EthSwap Instant Exchange";
+  Token public token;
+  uint public rate = 100;
+
+  constructor(Token _token) {
+    token = _token;
+  }
+
+  function buyTokens() public payable {
+    uint tokenAmount = msg.value * rate;
+    token.transfer(msg.sender, tokenAmount);
+  }
 }
